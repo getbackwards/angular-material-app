3fba03cb79b61beae392895d6fad178e99e8be68
getbackwards <tgray@tmmac002606.trademe.local>
2018-07-02 11:18:54 +1200

    Flex refresher part of the courde

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-
-import { AppComponent } from './app.component';
-import { MaterialModule} from './material.module';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CurrentTrainingComponent} from './training/current-training/current-training.component';
+import {LoginComponent} from './auth/login/login.component';
+import {NewTrainingComponent} from './training/new-training/new-training.component';
+import {MaterialModule} from './material.module';
+import {PastTrainingComponent} from './training/past-training/past-training.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {TrainingComponent} from './training/training.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CurrentTrainingComponent,
+    LoginComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
     SignupComponent,
-    LoginComponent
+    TrainingComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
+    BrowserModule,
     MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {
   MatButtonModule,
-  MatIconModule
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule],
+  exports: [MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule]
 })
 export class MaterialModule {
 }
